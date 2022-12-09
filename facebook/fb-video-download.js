fcf0fa0bb01aa7e92b49332c87759c4183039bf5
pa4080 <spas.z.spasov@gmail.com>
2022-12-09 13:27:48 +0200

    FB video download more instructions

--- facebook/fb-video-download.js
+++ facebook/fb-video-download.js
@@ -10,6 +10,10 @@
  *    or right-click on the page and select "Inspect".
  * 4. Click on the video to play it (then you can click again for pause).
  * 5. Copy and paste (Ctrl + V) the code below into the console and press Enter.
+ * 6. If the script returns an error like this: 
+ *    > VM220:5 GET https://scontent-sof1-2.xx.fbcdn.net/v/t39.25447-2/316093218_622565556273091_1337819706186371549_n.mp4?_...&_nc_rid=407503868866708 net::ERR_FAILED 200
+ *    Just right click on the URL and select open in new tab.
+ *    Then download the video from the three dots menu.
  * 
  * Reference: https://youtu.be/Ogt23VPBuQ8
  */
@@ -34,7 +38,7 @@ document.querySelectorAll("video").forEach((video, index) => {
             setTimeout(() => {
                 document.body.removeChild(downloadLink);
                 URL.revokeObjectURL(blobURL);
-            }, 100);
+            }, 500);
         })
         .catch((error) => `Video fetch error: ${error}`);
 });
